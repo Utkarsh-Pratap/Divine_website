3a66118de7f2e2ba72cf99d494481bc48663eee3
mukulkms <mukulkms.iimtgn@gmail.com>
2024-02-15 19:39:20 +0530

    new code

---
+++ gsap.js
@@ -0,0 +1,15 @@
+const imageContainer = document.getElementById('imageContainer');
+    const textHeading = document.getElementById('textHeading');
+    const textContent = document.getElementById('textContent');
+
+    const imageContent = [
+      { heading: 'YouTube', content: 'Mukul' },
+      { heading: 'Ironman', content: 'Utkarsh' },
+      { heading: 'Name', content: 'Content' }
+    ];
+
+    imageContainer.addEventListener('scroll', function() {
+      const currentImageIndex = Math.floor(this.scrollTop / this.clientHeight);
+      textHeading.innerHTML = `<p>${imageContent[currentImageIndex].heading}</p>`;
+      textContent.innerHTML = `<p>${imageContent[currentImageIndex].content}</p>`;
+    });
